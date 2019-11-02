e8b9961a924f59ee1cf48852beee54d634c6a047
Sunil <jadhav.sunil@gmail.com>
2019-11-03 08:33:58 +1100

    Fix: Updating Header and adding products to home screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+    width: 100%;
+    height: 60px;
+    color: #FFFFFF;
+    text-align: center;
+    background-color: #000000;
+`;
+
+const List = styled.ul`
+    padding: 20px;
+    margin: 0;
+`;
+
+const ListItem = styled.li`
+    float: left;
+    padding: 0;
+    list-style-type: none;
+`;
+
+const ListLink = styled(NavLink)`
+    color: #FFFFFF;
+    margin: 0 15px;
+    cursor: pointer;
+    text-decoration: none;
+`;
 
  const Header = () => {
     return(
-    <div>
-        <ul>
-            <li>
-              <NavLink  exact to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink  exact to="/cart">Cart</NavLink>
-            </li>
-        </ul>
+    <HeaderContainer>
+        <List>
+            <ListItem>
+              <ListLink  exact to="/">Home</ListLink>
+            </ListItem>
+            <ListItem>
+              <ListLink  exact to="/cart">Cart</ListLink>
+            </ListItem>
+        </List>
         
-    </div>
+    </HeaderContainer>
     )
  }
+
 export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
- const Home = () => {
-    return(
-    <div>
-        Home page
-    </div>
-    )
+ class Home extends Component {
+
+    handleClick = () => {
+        console.log('handle click....');
+    }
+
+    renderProduct = (product, index) => {
+        return(
+            <div key={'product'+ index}>
+                <div>
+                    <h2>{product.name}</h2>
+                    <h3>${product.price}</h3>
+                    <button onClick={()=>{this.handleAddToCartClick()}}>add to cart</button>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+
+        const products = [
+            {name: "Sledgehammer", price: 127.76 },
+            {name: "Axe", price: 190.51 },
+            {name: "Bandsaw",price: 562.14 },
+            {name: "Chisel", price: 13.9 },
+            {name: "Hacksaw", price: 19.45 }
+        ];
+
+        return(
+            <div>
+               {
+                    products.map((product, index) => this.renderProduct(product, index))
+                }
+            </div>
+            )
+    }
+
  }
+
 export default Home;
